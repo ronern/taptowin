8b0c924bc651d94b3d5f731e5a61cc6b3c9bd1e1
Dmitriy Volkov <hd.dmitry.volkov@gmail.com>
2020-10-22 14:12:13 +0300

    get info + register

--- go.mod
+++ go.mod
@@ -3,4 +3,6 @@ module github.com/ronern/taptowin
 go 1.12
 
 require (
+	github.com/heroku/x v0.0.26
+	github.com/jackc/pgx/v4 v4.9.0
 )
--- main.go
+++ main.go
@@ -1,15 +1,98 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	_ "github.com/heroku/x/hmetrics/onload"
+	"github.com/jackc/pgx/v4"
 	"log"
 	"net/http"
 	"os"
 )
 
-func hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "hello\n")
+var conn *pgx.Conn
+
+type Info struct {
+	Energy int32
+	Money  float32
+
+	EnergyTimer      uint64
+	EnergyVideoTimer uint64
+
+	Bet1    int32
+	Bet10   int32
+	Bet100  int32
+	Bet1000 int32
+}
+
+func getArg(req *http.Request, name string) string {
+	args, ok := req.URL.Query()[name]
+
+	if !ok || len(args[0]) < 1 {
+		return ""
+	}
+
+	return args[0]
+}
+
+func getInfoHandler(w http.ResponseWriter, req *http.Request) {
+
+	ids, ok := req.URL.Query()["id"]
+
+	if !ok || len(ids[0]) < 1 {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	id := ids[0]
+
+	var info Info
+
+	row := conn.QueryRow(context.Background(), "select energy, money, bet1, bet10, bet100, bet1000, energyTimer, energyVideoTimer from users where id=$1", id)
+	err := row.Scan(&info.Energy, &info.Money, &info.Bet1, &info.Bet10, &info.Bet100, &info.Bet1000, &info.EnergyTimer, &info.EnergyVideoTimer)
+
+	if err == pgx.ErrNoRows {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	infoJson, err := json.Marshal(info)
+
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprint(w, infoJson)
+}
+
+func registerHandler(w http.ResponseWriter, req *http.Request) {
+
+	var id string
+	var name string
+
+	id = getArg(req, "id")
+	name = getArg(req, "name")
+
+	if len(id) == 0 || len(name) == 0 {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	_, err := conn.Exec(context.Background(), "INSERT INTO users (id, name) VALUES ('$1', '$2') ON CONFLICT DO NOTHING", id, name)
+
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprint(w, "OK")
 }
 
 func headers(w http.ResponseWriter, req *http.Request) {
@@ -22,13 +105,22 @@ func headers(w http.ResponseWriter, req *http.Request) {
 
 func main() {
 
+	var err error
+	conn, err = pgx.Connect(context.Background(), "postgres://tnqhqsdfdamjoa:47e01ba2a9d708fa5dcdf10653b64e7c588ca9eef2befdf3415a8eaee4d318d2@ec2-54-75-225-52.eu-west-1.compute.amazonaws.com:5432/d516nrb1815o1")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	//defer conn.Close(context.Background())
+
 	port := os.Getenv("PORT")
 
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
 
-	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/info", getInfoHandler)
+	http.HandleFunc("/register", registerHandler)
 	http.HandleFunc("/headers", headers)
 	http.ListenAndServe(":"+port, nil)
 }
